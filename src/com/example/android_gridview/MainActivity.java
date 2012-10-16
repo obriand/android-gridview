d522901a64cd44f50c6d9068a96de3571374d9e2
olivier briand <olivier.briand@orange.com>
2012-10-16 23:01:05 +0200

    Add click on an item

--- src/com/example/android_gridview/MainActivity.java
+++ src/com/example/android_gridview/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android_gridview;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
 public class MainActivity extends Activity {
@@ -17,6 +20,22 @@ public class MainActivity extends Activity {
         // Instance of ImageAdapter Class
         ImageAdapter imageAdapter = new ImageAdapter(this.getApplicationContext());        
         gridView.setAdapter(imageAdapter);
+        
+        /**
+         * On Click event for Single Gridview Item
+         * */
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,int position, long id) {
+ 
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+        
     }
 
     @Override

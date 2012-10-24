021ab8bf545cfcd9265c7e5379bd276ff7e8ea6b
olivier briand <olivier.briand@orange.com>
2012-10-24 21:15:55 +0200

    Set functionnal URLs for both UIL and Custo cases

--- src/com/example/android_gridview/DynUILGridActivity.java
+++ src/com/example/android_gridview/DynUILGridActivity.java
@@ -29,7 +29,7 @@ public class DynUILGridActivity extends Activity {
         mImageUrls.add("http://static2.dmcdn.net/static/video/501/543/28345105:jpeg_preview_large.jpg?20120507124341");
         mImageUrls.add("http://static2.dmcdn.net/static/video/001/543/28345100:jpeg_preview_large.jpg?20120507124341");
         mImageUrls.add("http://static2.dmcdn.net/static/video/970/543/28345079:jpeg_preview_large.jpg?20120507124341");
-        mImageUrls.add("http://sstatic2.dmcdn.net/static/video/170/543/28345071:jpeg_preview_large.jpg?20120507124341");
+        mImageUrls.add("http://static2.dmcdn.net/static/video/170/543/28345071:jpeg_preview_large.jpg?20120507124341");
         
         mImageLoader.init(ImageLoaderConfiguration.createDefault(this));
         
